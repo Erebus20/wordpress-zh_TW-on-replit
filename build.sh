e6b961743b84468389d2edf0ad3e92db0c9487e6
Erebus20 <129663203+Erebus20@users.noreply.github.com>
2023-04-05 01:12:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 nix-env -iA nixpkgs.wget
 mkdir build
 cd build
-wget https://github.com/sxbai/wordpress-zh_CN-on-replit/raw/main/php.zip
+wget https://github.com/Erebus20/wordpress-zh_TW-on-replit/raw/main/php.zip
 nix-env -iA nixpkgs.unzip
 unzip php.zip
 cd ..
-wget -O install.sh https://github.com/sxbai/wordpress-zh_CN-on-replit/raw/main/install.sh
+wget -O install.sh https://github.com/Erebus20/wordpress-zh_TW-on-replit/raw/main/install.sh
 cp -r build/.replit . && cp -r build/replit.nix .
 cp -r build/.cache .cache
 rm -rf build/
